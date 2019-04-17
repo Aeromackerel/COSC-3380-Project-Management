459f852dd9a9247c7149b6017719c1cbd187a43a
ianvoorhies <49426127+ianvoorhies@users.noreply.github.com>
2019-04-17 03:01:54 -0500

    Update projectManagersIndex.php

--- front-end/projectManagers/projectManagersIndex.php
+++ front-end/projectManagers/projectManagersIndex.php
@@ -27,18 +27,19 @@ if (!$_SESSION['loggedin'] || $_SESSION['roleID'] != 3)
 		</div> 
 	</div>
 	<table id="applink-table">
+		
 		<tr>
-		<td class="applink-td" style="height:200px"><a href="../timesheets/timesheet.php" class="applink">Timesheet</a></td>
-		<td class="applink-td" style="height:200px"><a href = "projectManagersEmployeeReport.php" class="applink"> Employees Report </a> </td>
-		<td class="applink-td" style="height:200px"><a href = "projectManagersCostReport.php" class= "applink"> Project costs Report </a> </td>
-		<td class="applink-td" style="height:200px"><a href = "projectManagersHoursReport.php" class= "applink"> Project hours Report </a> </td>
+		<td class="applink-td" style="height:250px"><a href="../timesheets/timesheet.php" class="applink">Timesheet</a></td>
+		<td class="applink-td" style="height:250px"><a href = "projectManagersEmployeeReport.php" class="applink"> Employees Report </a> </td>
+		<td class="applink-td" style="height:250px"><a href = "projectManagersCostReport.php" class= "applink"> Project costs Report </a> </td>
+		<td class="applink-td" style="height:250px"><a href = "projectManagersHoursReport.php" class= "applink"> Project hours Report </a> </td>
 		</tr>
 
 		<tr>
-		<td class="applink-td" style="height:200px"><a class="applink" href = "projectManagersTasks.php"> Tasks </a></td>
-		<td class="applink-td" style="height:200px"><a class="applink" href = "projectManagersEvents.php"> Events </a></td>
-		<td class="applink-td" style="height:200px"><a class="applink" href = "projectManagersGroups.php"> Groups </a></td>
-		<td class="applink-td" style="height:200px"><a class="applink" href = "projectManagersEmployees.php"> Employees </a></td>
+		<td class="applink-td" style="height:250px"><a class="applink" href = "projectManagersTasks.php"> Tasks </a></td>
+		<td class="applink-td" style="height:250px"><a class="applink" href = "projectManagersEvents.php"> Events </a></td>
+		<td class="applink-td" style="height:250px"><a class="applink" href = "projectManagersGroups.php"> Groups </a></td>
+		<td class="applink-td" style="height:250px"><a class="applink" href = "projectManagersEmployees.php"> Employees </a></td>
 		</tr>
 	</table>
 </body>
