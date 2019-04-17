15fa72cc9bc4ccd38676339b3154648421152b98
greg-beck <greg.c.beck@gmail.com>
2019-04-16 22:08:09 -0500

    Update groupManagersIndex.php

--- front-end/groupManagers/groupManagersIndex.php
+++ front-end/groupManagers/groupManagersIndex.php
@@ -3,7 +3,7 @@
 <?php
 session_start();
 
-if (!$_SESSION['loggedin'] || $_SESSION['roleID'] != 2) 
+if (!$_SESSION['loggedin'] || $_SESSION['roleID'] != 2)
 {header ("Location: ../login.php");}
 
 
@@ -20,11 +20,23 @@ if (!$_SESSION['loggedin'] || $_SESSION['roleID'] != 2)
 		<div class="nav">
 		   <button class="nav-hover">Menu</button>
 		   <div class="nav-links">
-				<a href="../timesheets/01262019.html">timesheet</a>
-				<a href="../actionLogOut.php">sign out</a>
+				<a href="../timesheets/01262019.html">Timesheet</a>
+				<a href="../actionLogOut.php">Sign out</a>
 			</div>
-		</div> 
+		</div>
 	</div>
+
+	<table id="applink-table">
+		<tr>
+		<td class="applink-td"><a href="../timesheets/01262019.html" class="applink">Timesheet</a></td>
+		<td class="applink-td"><a class="applink" href = "groupManagersTimeApprovals">Timesheet Approvals</a></td>
+		<td class="applink-td"><a class="applink" href = "groupManagersTask.php">Tasks</a></td>
+		<td class="applink-td"><a class="applink" href = "groupManagersTasksOverview.php"> Tasks Report</a></td>
+		<td class="applink-td"><a class="applink" href = "groupManagersEventsView.php">Events</a></td>
+		<td class="applink-td"><a class="applink" href = "groupManagersGroupsView.php">Groups</a></td>
+		</tr>
+	</table>
+	<!---
 	<a id="applink" href="../timesheets/01262019.html"> Timesheet</a>
 	<a id = "applink" href = "groupManagersTimeApprovals"> Timesheet approvals </a>
 	<a id="applink" href = "groupManagersTask.php"> Tasks </a>
@@ -32,6 +44,6 @@ if (!$_SESSION['loggedin'] || $_SESSION['roleID'] != 2)
 	<a id="applink" href = "groupManagersEventsView.php"> Events </a>
 	<a id="applink" href = "groupManagersGroupsView.php"> Groups </a>
 	<a id ="applink"> Projects</a>
-
+	--->
 </body>
 </html>
