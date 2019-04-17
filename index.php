ce051ac1cf1501a2023aad14392982d3c694c782
Aeromackerel <36085800+Aeromackerel@users.noreply.github.com>
2019-04-16 19:17:52 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<title>Home Page</title>
+<link rel ="stylesheet" type="text/css" href="front-end/users/bootstrap.css">
+<link rel="stylesheet"type="text/css"href="front-end/style.css">
+<body background= "front-end/images/initialBG.jpg">
+	<div id="header" class="ui-container">
+		<div class="nav">
+		   <button class="nav-hover">Menu</button>
+		   <div class="nav-links">
+				<a href="index.php">Home</a>
+				<a href="front-end/login.php">Login</a>
+				<a href="front-end/register.php">Register</a>
+			</div>
+		</div> 
+	</div>
+
+</body>
+
+</html>
