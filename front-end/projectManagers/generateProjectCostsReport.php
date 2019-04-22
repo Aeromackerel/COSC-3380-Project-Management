bb33095a04f5ca42debb62c0921d5f514f9cfc9d
Aeromackerel <36085800+Aeromackerel@users.noreply.github.com>
2019-04-22 15:47:47 -0500

    Add files via upload

---
+++ front-end/projectManagers/generateProjectCostsReport.php
@@ -0,0 +1,70 @@
+<!----- PHP HEADER ----->
+
+<?php
+session_start();
+if (!$_SESSION['loggedin'] || $_SESSION['roleID'] != 3) 
+{header ("Location: ../login.php");}
+// Store session
+$tempUserID = (int)$_SESSION['userID'];
+$roleID = (int)$_SESSION['roleID'];
+$projectId = (int)$_GET['report'];
+// Include DB Connection
+include "../../../includes/dbconnect.ini.php";
+//Total Budget
+$sqlTotalBudgetQuery = "SELECT totalBudget FROM ProjectCosts";
+$stmt1 = $conn->query($sqlTotalBudgetQuery);
+$row1 = $stmt1->fetch(PDO::FETCH_ASSOC);
+$budgetTotal = $row1['totalBudget'];
+//Total Costs
+//-----current issue is with the sql statement. Just need to add both columns and spit it out`
+$sqlTotalCostsQuery = "SELECT tasksCosts + wagesCosts as totalCost From ProjectCosts";
+$stmt3 = $conn->query($sqlTotalCostsQuery);
+$row3 = $stmt3->fetch(PDO::FETCH_ASSOC);
+$costsTotal = $row3['totalCost'];
+?>
+
+<!----- HTML SECTION ----->
+
+<!DOCTYPE HTML>
+<title>Project Costs Report</title>
+<link rel="stylesheet"type="text/css"href="../../users/bootstrap.css">
+<link rel="stylesheet"type="text/css"href="../../style.css">
+<body>
+	<div id="header" class="ui-container">
+		<div class="nav">
+		   <button class="nav-hover">Menu</button>
+		   <div class="nav-links">
+				<a href="../projectManagersIndex.php"> Back to Index </a>
+				<a href="../../actionLogOut.php">Sign out</a>
+			</div>
+		</div> 
+	</div>
+
+	<div id = "login-container" class = "ui-container">
+
+	<?php
+	// Include DB Connection
+	include "../../../includes/dbconnect.ini.php";
+	$sqlProjectNameQuery = "SELECT projectName FROM Projects WHERE projectId = $projectId";
+	$stmt = $conn->query($sqlProjectNameQuery);
+	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+	echo "<center><p> Project Costs Report on $row[projectName]</p></center>"
+	?>
+
+	<center> <label> Total Budget </label> </center>
+	<center>
+		<?php 
+			echo "<p>".$budgetTotal."</p>";
+		?>
+	</center>
+
+	<center> <label> Total Costs </label> </center>
+	<center>
+		<?php 
+			echo "<p>".$costsTotal."</p>";
+		?>
+	</center>
+	</div>
+
+</body>
+</HTML>
--- front-end/projectManagers/projectManagersCostReport.php
+++ front-end/projectManagers/projectManagersCostReport.php
@@ -2,26 +2,16 @@
 
 <?php
 session_start();
-
 if (!$_SESSION['loggedin'] || $_SESSION['roleID'] != 3)
 {header ("Location: ../login.php");}
-
 $tempUserID = $_SESSION['userID'];
 $roleID = (int)$_SESSION['roleID'];
-
-
 // Takes them back to the index
-
 if (isset($_POST['goBack']))
 {header ("Location: projectManagersIndex.php");}
-
 // If submit - then we take the user to another page to generate a Report on the given project ID
-
 else if (isset($_POST['submit']))
-{header ("Location: actions/generateProjectCostsReport.php?report=$_POST[relatedProjectGen]&from=$_POST[fromDateCreate]&to=$_POST[toDateCreate]");}
-
-
-
+{header ("Location: actions/generateProjectCostsReport.php?report=$_POST[relatedProjectGen]");}
 ?>
 
 <!DOCTYPE HTML>
@@ -47,47 +37,26 @@ else if (isset($_POST['submit']))
 			<!----- Query for projects that a user belongs to ----->
 
 			<?php
-
 				// Initialize DB connection
-
 				include "../../includes/dbconnect.ini.php";
-
 				// Query for projects that a user is assigned to
-
 				$sqlProjectsUser = "SELECT DISTINCT projectId FROM ProjectUsers WHERE employeeId = $tempUserID";
-
 				$stmtFindProjects = $conn->query($sqlProjectsUser);
-
 				while ($rowProjectID = $stmtFindProjects->fetch(PDO::FETCH_ASSOC))
 				{
-
 				// Query for projects that a user might belong to
-
 				$sqlFindProjectsUser = "SELECT DISTINCT Projects.projectId, Projects.projectName FROM Projects INNER JOIN ProjectUsers ON Projects.projectId = $rowProjectID[projectId] ";
-
 				$stmtFindProject = $conn->query($sqlFindProjectsUser);
 				while ($rowProject = $stmtFindProject->fetch(PDO::FETCH_ASSOC))
 				{
 					unset($projectIdOption, $projectNameOption);
 					$projectIdOption = $rowProject['projectId'];
 					$projectNameOption = $rowProject['projectName'];
-
 					 echo '<option value="'.$projectIdOption.'">'.$projectNameOption.'</option>';
-
 				}
 			}
-
-
 			?>	
 		</select>
-      		<center>
-				<label for = "fromDate">  From:   </label>
-	 		   	<input type = "date" class = "form-control" name= "fromDateCreate" placeholder = "2019-05-01">
-			</center>
-			<center>
-				<label for = "toDate">  To:  </label>
-				<input type = "date" class = "form-control" name= "toDateCreate" placeholder = "2019-05-01">
-			</center>
 			 
 		<center>
       	<button type="submit" name = "goBack" class="btn btn-secondary btn-space2">Back</button>
