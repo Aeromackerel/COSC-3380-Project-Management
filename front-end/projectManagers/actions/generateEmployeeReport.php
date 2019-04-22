8eddaec64300c68f5ed5e34c781dd960c10d6573
ianvoorhies <49426127+ianvoorhies@users.noreply.github.com>
2019-04-22 01:59:30 -0500

    Update generateEmployeeReport.php

--- front-end/projectManagers/actions/generateEmployeeReport.php
+++ front-end/projectManagers/actions/generateEmployeeReport.php
@@ -144,8 +144,10 @@ chart.render();
 
 
 	</div>
+	<div id="chartContainerContainer">
 	<div id="chartContainer" style="height: 370px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+	</div>
 
 
 </body>
